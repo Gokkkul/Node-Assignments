5e3cec9d92093aa5af489b302ca1745f9398cbd0
Lavanya Satpute IN Dev <lavanya.satpute@jibe.co.in>
2025-03-18 10:15:33 +0530

    add files

--- src/controllers/login.controller.ts
+++ src/controllers/login.controller.ts
@@ -5,7 +5,8 @@ class LoginController{
     async loginUser(req:Request, res:Response){
         const data = req.body;
         const result = await LoginService.loginUser(data.name,data.email);
-        
-
+        res.send(result);
     }
 }
+
+module.exports = new LoginController()
